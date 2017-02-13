57c7af23773637d75425741cd8f02163e6c0149d
sdll <shura.sdll@gmail.com>
2017-02-13 19:46:49 +0000

    170213

---
+++ ARBEIT/auto/ARBEIT20170201MAT247_Pset+III_3.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "ARBEIT20170201MAT247_Pset+III_3"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("scrartcl" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("masty" "beaue" "pset" "anon")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "scrartcl"
+    "scrartcl11"
+    "masty"
+    "lineno")
+   (TeX-add-symbols
+    '("lr" 1)
+    '("R" 1)
+    '("mbt" 1)
+    '("mbv" 1)
+    '("mbn" 1)
+    '("mbh" 1)
+    "LongestHence"
+    "LongestName"
+    "LongestValue"
+    "LongestText")
+   (LaTeX-add-labels
+    "#1")
+   (LaTeX-add-lengths
+    "LargestHenceSize"
+    "LargestNameSize"
+    "LargestValueSize"
+    "LargestTextSize"))
+ :latex)
+
