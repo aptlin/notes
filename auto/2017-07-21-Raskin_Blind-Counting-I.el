b1e42a62f7a99ce67dcd271d50bec85f36836e8a
sdll <shura.sdll@gmail.com>
2017-07-22 21:23:24 +0300

    170722

---
+++ auto/2017-07-21-Raskin_Blind-Counting-I.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "2017-07-21-Raskin_Blind-Counting-I"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("scrartcl" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("masty" "fancy" "beaue" "pset" "anon")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "scrartcl"
+    "scrartcl11"
+    "masty"
+    "lineno")
+   (TeX-add-symbols
+    '("lr" 1)
+    '("R" 1)
+    '("mbt" 1)
+    '("mbv" 1)
+    '("mbn" 1)
+    '("mbh" 1)
+    "LongestHence"
+    "LongestName"
+    "LongestValue"
+    "LongestText")
+   (LaTeX-add-labels
+    "#1")
+   (LaTeX-add-lengths
+    "LargestHenceSize"
+    "LargestNameSize"
+    "LargestValueSize"
+    "LargestTextSize"))
+ :latex)
+
